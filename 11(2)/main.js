bdf13acc9fd5f61d80ffc97cf8e624eabdd68121
soapcnsos-hub <sasabelmesov@gmail.com>
2026-05-13 09:03:21 +0500

    Delete 11(2) directory

--- 11(2)/main.js
+++
@@ -1,9 +0,0 @@
-const buttonMenu = document.querySelector(".mobile-navigation__header-button");
-const mobileNavigation = document.querySelector(".mobile-navigation");
-
-
-buttonMenu.addEventListener("click", buttonMenuClicked);
-
-function buttonMenuClicked() {
-    mobileNavigation.classList.toggle('opened')
-}
