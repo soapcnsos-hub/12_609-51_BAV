1f7b71680a1d17096412ce86ab0934acdbfe09d7
soapcnsos-hub <sasabelmesov@gmail.com>
2026-05-13 09:02:58 +0500

    Add files via upload

---
+++ nexcent0/main.js
@@ -0,0 +1,9 @@
+const buttonMenu = document.querySelector(".mobile-navigation__header-button");
+const mobileNavigation = document.querySelector(".mobile-navigation");
+
+
+buttonMenu.addEventListener("click", buttonMenuClicked);
+
+function buttonMenuClicked() {
+    mobileNavigation.classList.toggle('opened')
+}
